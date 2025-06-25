d9221ae57bda90ec72f17fd30f82b0d9e24755bd
Epicmanpreet02 <144267040+Epicmanpreet01@users.noreply.github.com>
2025-06-25 21:39:53 +0530

    small change in generateToken

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -9,6 +9,6 @@ export const generateTokenAndSetCookie = async (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000, //MS
     httpOnly: true,
     sameSite: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === "production",
   });
 };
