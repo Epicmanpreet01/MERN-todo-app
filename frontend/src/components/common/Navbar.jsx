8330ffc8094f325f1110b6e50785d89b43f601bb
Epicmanpreet02 <144267040+Epicmanpreet01@users.noreply.github.com>
2025-06-25 23:31:30 +0530

    completed ui for navbar

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import TaskMateLogo from '../svgs/logo';
+import { Link } from 'react-router-dom';
+import { Sunsvg } from '../svgs/sun.jsx';
+import { MoonSvg } from '../svgs/moon.jsx';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar bg-base-100 shadow-sm flex justify-between px-10">
+      <Link to={'/'}>
+        <TaskMateLogo />
+      </Link>
+      <div className='flex space-x-5 '>
+      <label className="swap swap-rotate">
+        {/* this hidden checkbox controls the state */}
+        <input type="checkbox" className="theme-controller" value="synthwave" />
 
-export default Navbar
+        {/* sun icon */}
+        <Sunsvg />
+
+        {/* moon icon */}
+        <MoonSvg />
+      </label>
+        <div className="avatar">
+          <div className="ring-primary ring-offset-base-100 w-11 rounded-full ring-2 ring-offset-2 cursor-pointer">
+            <img src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
